ca9a72491123071b22d8072bdc09a2326bc8534d
Mike-code1805 <miguelenrique1805@gmail.com>
2023-12-14 16:33:06 -0500

    middleware encrypt

--- 3.js
+++ 3.js
@@ -105,27 +105,224 @@ function encrypt(textToEncrypt) {
   }
 }
 
-encrypt('Este texto lo voy a encriptar ahora');
+encrypt(
+  JSON.stringify({
+    usr: 'sa',
+    pwd: 'Fsz3nQ3PZgbA2i',
+    nbd: 'PALERPreniec',
+    proc: 'USP_PRI_RENIEC_GSimple',
+    parameter: [
+      {
+        1: 'uno',
+      },
+      {
+        2: 'dos',
+      },
+      {
+        3: 'tres',
+      },
+      {
+        4: 'cuatro',
+      },
+      {
+        5: 'cinco',
+      },
+      {
+        6: 'seis',
+      },
+      {
+        7: 'siete',
+      },
+      {
+        8: 'ocho',
+      },
+      {
+        9: 'nueve',
+      },
+      {
+        10: 'diez',
+      },
+      {
+        11: 'once',
+      },
+      {
+        12: 'doce',
+      },
+      {
+        13: 'trece',
+      },
+      {
+        14: 'catorce',
+      },
+      {
+        15: 'quince',
+      },
+      {
+        16: 'dieciseis',
+      },
+      {
+        17: 'diecisiete',
+      },
+      {
+        18: 'dieciocho',
+      },
+      {
+        19: 'diecinueve',
+      },
+      {
+        20: 'veinte',
+      },
+      {
+        21: 'veintiuno',
+      },
+      {
+        22: 'veintidos',
+      },
+      {
+        23: 'veintitres',
+      },
+      {
+        24: 'veinticuatro',
+      },
+      {
+        25: 'veinticinco',
+      },
+      {
+        26: 'veintiseis',
+      },
+      {
+        27: 'veintisiete',
+      },
+      {
+        28: 'veintiocho',
+      },
+      {
+        29: 'veintinueve',
+      },
+      {
+        30: 'treinta',
+      },
+      {
+        31: 'treinta y uno',
+      },
+      {
+        32: 'treinta y dos',
+      },
+      {
+        33: 'treinta y tres',
+      },
+      {
+        34: 'treinta y cuatro',
+      },
+      {
+        35: 'treinta y cinco',
+      },
+      {
+        36: 'treinta y seis',
+      },
+      {
+        37: 'treinta y siete',
+      },
+      {
+        38: 'treinta y ocho',
+      },
+      {
+        39: 'treinta y nueve',
+      },
+      {
+        40: 'cuarenta',
+      },
+      {
+        41: 'cuarenta y uno',
+      },
+      {
+        42: 'cuarenta y dos',
+      },
+      {
+        43: 'cuarenta y tres',
+      },
+      {
+        44: 'cuarenta y cuatro',
+      },
+      {
+        45: 'cuarenta y cinco',
+      },
+      {
+        46: 'cuarenta y seis',
+      },
+      {
+        47: 'cuarenta y siete',
+      },
+      {
+        48: 'cuarenta y ocho',
+      },
+      {
+        49: 'cuarenta y nueve',
+      },
+      {
+        50: 'cincuenta',
+      },
+      {
+        51: 'cincuenta y uno',
+      },
+      {
+        52: 'cincuenta y dos',
+      },
+      {
+        53: 'cincuenta y tres',
+      },
+      {
+        54: 'cincuenta y cuatro',
+      },
+      {
+        55: 'cincuenta y cinco',
+      },
+      {
+        56: 'cincuenta y seis',
+      },
+      {
+        57: 'cincuenta y siete',
+      },
+      {
+        58: 'cincuenta y ocho',
+      },
+      {
+        59: 'cincuenta y nueve',
+      },
+      {
+        60: 'sesenta',
+      },
+      {
+        61: 'sesenta y uno',
+      },
+      {
+        62: 'sesenta y dos',
+      },
+      {
+        63: 'sesenta y tres',
+      },
+      {
+        64: 'sesenta y cuatro',
+      },
+      {
+        65: 'sesenta y cinco',
+      },
+      {
+        66: 'sesenta y seis',
+      },
+      {
+        67: 'sesenta y siete',
+      },
+      {
+        68: 'sesenta y ocho',
+      },
+    ],
+  })
+);
 
-const crypto = require('crypto');
-
-function generateKeyPair() {
-  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 2048,
-    publicKeyEncoding: {
-      type: 'spki',
-      format: 'pem',
-    },
-    privateKeyEncoding: {
-      type: 'pkcs8',
-      format: 'pem',
-    },
-  });
-
-  return { publicKey, privateKey };
-}
-
-// Ejemplo de uso
-const { publicKey, privateKey } = generateKeyPair();
-// console.log('Public Key:\n', publicKey);
-// console.log('\nPrivate Key:\n', privateKey);
+decrypt({
+  privateKey:
+    'U2FsdGVkX189xbmynsoAKVA3J4h4Qpc2xsrmWMNlv50vNm7ssy9F4+LvKDCE9QRtXUP+++wmVBLMpkBX963TriceOSK0L8n2D6AbmSWFpURpwsxQpVG+Zxhpi+uB8rMxSshUGMKk1VyZL6tgd3Qw7WpGYxfr3/aoea3PCL/TRm8JVg0FFbke7q8PeqzWAESLJJLOWGD+8gfh1Gpl2V1fe8g7alJgCM9E3nZqiOG+bHjCUqZ/4V583CCVFHGAMlOq/g155Qb4/B1O8PUVRaGTlgx4Pigs7BYbtHlNeA/h9ssuNL+8kNUT5tgVTE7bvwcW6ogn2Qdp99QiYjpf1ex8KVTjsIY32WEsEOyBzX1mYoLDj7czqE0oOfnCdPRWn53+Nom35iKLlBlhRD6BFJUFyTDqOUB/n+4dgVffHg5iu4k2E9JgTKMCrAvyKT9jKmmxvQ8YEZTGdGCzyhfsZetBtNWO8QmFXzJa8omvhFZqAHqRjtgIAufVBxxoOk5lhiosshspSORI0aFFJSRFf42mFAC+CYdnA+//emXIbGQ9ONtSK8n9hl0OeqPW7sA0QSp8uyIUzzNvb2X6JGXfMVg8N84+TnILsnSrIuca5Pa0vJgM0MFoIez485nb778/Qit3Jr7v3vIIN9kKnceFYsRKYrGg5FwfEP+kxXcXIsthrYRBkvXyffyxkJPM0IaxMFkYEdlTMN1kon2eCdrFHEpcRKotZ7TcVbT1nPeRLV7guFahdkU54apbE5jX6Ok9rnAJ4KW8LsB6hCl3n/5ilWBLRYCqYH/eScMm7YcWucMpfhbMIpgDKjt5MzAXm8Io1JmeBhqWeSZcAkvPyuNXtzpUdMIctSH+babMYVskI2v9N5bKUBqBF6qbVtf4vEjURIfr793fac/3rwfoHth7yfe+6t59X8vZC10hoDkVMQM3jL36VJXXzaiIePz3HJB/XdxUAqA/AbDFF0YkZWDdc3QuqapuOK4q+B1+Q/oON5VRkpXrCd8cUQO3forwPfrY6e9bPHMgnpl1AOPOJbVc5MYimz8WD/qhDNZdPoCxKlAIaaumtqCZBux6J46BgBCjMcKfhe1MJA+rTU2XBPcIxSYB76i5i5ZE+axCieTf4gEHV0SVvImb9smEzmgK+jHuN1gkIAlvkepcAhlKjfU6og/8/17godjegrvSDDlN23AcBDhPGWtHRS/cNh1n9LSScth15XvfYBhbO5/k3Oel4t9J1FD4TYzHGdCesUZSV9v1OK+notdIP+2LNqGlhoESn5/23B3x4u8P7Ks8CHgZAog0xUmMFOCaOyK9vI2oURWBOtfZZme5ZA+sN9LaAiquFT+OKf6cO8vgnrahunkvIEpf3s8cSp+XXInlCzKGuv8TOkQLiFhgVdDRVmg01kTBJA4ZHpCWMfQzYMLox3BUJktlSebJ3ZNEIJLpJMkBxYFHlRGKSYtmmfLnWS5+qc5cahiyM1oyYZ5Mqoy25u+RcZozanp1eBCWyeZ5ZfFEiXYSa3jHhqIKJtx9/8HhkNlkTABuNBycbxS3rS2DYhV11hpBHQ3y/tpovEkCk5srSFxdXthRFNJ68Ehk3aFieXRDS03WjZjB8GwLZZJ0eJXFBHArTQ6EEWuD75oiFMl1k6wWGaBZJw9Tv19QkSMDrtOYw9y1hdZGLUQArDWTwMIUMh9reXr0CwjO6UNNo0F7H0sA2IpSdh6add/Qa3a0bzqTyGUDE1YkFe3Wj6jsfBX6GMmUBXIlrgISkuEG7CevPYs0HV/kshA6P+x43rSqwZrZBBgC2Jp9Z7HFs/thYpJsKXygNMguesGRP43p/Qr2U41nIJh9SCg1K+Q3j7+IE4IQJHuDB/swIWzLFPzdKBar7DEoVF/Wx8IeVMTgHA8zs/F8nTZwLPqUh41hV+vgipTwTcIdpIFwRaMKUm7VPrVVpPTl1990IW6MCgqWD46ldUc6uNZNH1ZrYg5a1v5fHcunZZqY9Fih++bA65i16XA/TW+Ffklbj9rGWlvD4T2KZNJQFHEKkATPLWlPJw+Z+v61kO8/jFq/jyQLKMBfBH1cBVY3asEpzWXFM7JL/dG86y9YOknYNN/+IuDxqZAzV/KGUrNfrsLJpT/7jCqJsH6/jqi4Ya8NY6P65r9K9GScjwmOcBRK0oFNaKGcx7+iU2lUgOXke92FuVK6WGjeCHu6dNxh09SQwWt/z2XgGL3/49NN0Ln4qvZhfL49yagd6KP7Pgd2AlKknM1PiAa5crCrEw/G5g==',
+  textToEncrypt:
+    'pUYeiIXtJo4TXMBlm0rSkdZh86mZUe9Ndc7R3nh3oBOyePZkJxoiFrPm/qx7iVFUa4qGGNvUWu4zDQtkpYsnLlmRuBg0P0s82GI7ZMMjLyOq6MvpyETlbPmiIQJT7fDWMEEM5CxOFI9k2ct0Jgyebb7CCw04Qytpek4jOGvFXROyogd1832SJ8LGAr4CEPzbvuxz8/RqsSXfZB2rKCQR9WT1qDlkXbKhA8wELRKcN9x6kKAt4wvWSGoKrx4tmJl6u3T9d3VfEU/uXt5UHrGe0VW38Mit3FYgclb8w875WXumx61+UXbFpsWedJSJhFBFzFi8DxGiL4fuaOQI2OP3aQ==',
+});
--- constants.js
+++ constants.js
@@ -1,7 +1,13 @@
 const crypto = require('crypto');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // Función para encriptar con la llave pública
 function encryptWithPublicKey(publicKey, data) {
+  console.log('encryptWithPublicKey');
+  console.log('encryptWithPublicKey');
+  console.log({ publicKey });
   const bufferData = Buffer.from(JSON.stringify(data), 'utf-8');
   const encryptedData = crypto.publicEncrypt(publicKey, bufferData);
   return encryptedData.toString('base64');
@@ -14,15 +20,64 @@ function decryptWithPrivateKey(privateKey, encryptedData) {
   return JSON.parse(decryptedData.toString('utf-8'));
 }
 
-// Ejemplo de generación de claves
+// // Ejemplo de generación de claves
 const keyPair = crypto.generateKeyPairSync('rsa', {
   modulusLength: 2048,
   publicKeyEncoding: { type: 'spki', format: 'pem' },
   privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
 });
+console.log({ keyPair });
+console.log(process.env.PUBLIC_KEY);
 
-const publicKey = keyPair.publicKey;
-const privateKey = keyPair.privateKey;
+const keys = {
+  keyPair: {
+    publicKey:
+      '-----BEGIN PUBLIC KEY-----\n' +
+      'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw/rz/P9GCeF6yTlP+BJ0\n' +
+      'fRKKPOL1OJ7ngBIka1Y6cQtwQ/OZZaSUNzK36caNmnB/nxZh4g389ill6RaH/tp7\n' +
+      'rpsScELYDTLQyfrOERsijqYGjdfnI2ACIYrSUTmQjTFLiSOgX8frGacQWGqurhoX\n' +
+      '1Zklu3/ddarahlzA0WkfOL+DK0PfQmHDcl4ju9gFjMoO2b13oiPYJONBFy0oWsMN\n' +
+      'Iif5h58i5sj26BpEOgH8yD1Jut1FlLFPxKFUQYIHYGoeXZJlMDw9eSnGOAhMMsb2\n' +
+      'RFk53fjtkS+TfYPw/5lNRpN44TYEew8O8HWGM2Vi6r828c5RmHdEUPYa1eeqxVh9\n' +
+      'uwIDAQAB\n' +
+      '-----END PUBLIC KEY-----\n',
+    privateKey:
+      '-----BEGIN PRIVATE KEY-----\n' +
+      'MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDD+vP8/0YJ4XrJ\n' +
+      'OU/4EnR9Eoo84vU4nueAEiRrVjpxC3BD85llpJQ3Mrfpxo2acH+fFmHiDfz2KWXp\n' +
+      'Fof+2nuumxJwQtgNMtDJ+s4RGyKOpgaN1+cjYAIhitJROZCNMUuJI6Bfx+sZpxBY\n' +
+      'aq6uGhfVmSW7f911qtqGXMDRaR84v4MrQ99CYcNyXiO72AWMyg7ZvXeiI9gk40EX\n' +
+      'LShaww0iJ/mHnyLmyPboGkQ6AfzIPUm63UWUsU/EoVRBggdgah5dkmUwPD15KcY4\n' +
+      'CEwyxvZEWTnd+O2RL5N9g/D/mU1Gk3jhNgR7Dw7wdYYzZWLqvzbxzlGYd0RQ9hrV\n' +
+      '56rFWH27AgMBAAECggEAGEIH0VkHolF2kVG2xE4EfTa+RasSD2Ab545XU7HoqfVw\n' +
+      'f/i3FA27CtoMSbN585PZOCfdFc4FcbLOGnvfeXwxsvse3SZo97lCfr28pAFn1aBX\n' +
+      'T1daFNbdbKlyPvuRGOjxIJh4UWK9hv9W1prd5E72hEngkcTR9K6hPeBdPY51fuB+\n' +
+      'EAZdLp3/4mgz7ZsylqOcNlIugPXYlq1rXfdedOIJ/T7FMigRGUlhghHRG5oEBd/j\n' +
+      'p1jsO8NgybbaOcFxXiMaRcptAKYAjRRuezYo/TBggYPERkbEWpaVavs0nv+g0DzR\n' +
+      'ihO6ir7AC0etcCg5I1ow1pK/GpPqPJj6YZnkxceCgQKBgQDxleD+s2FQ8ivgOmOp\n' +
+      'Sf79RAJ3oEBHONzNEJwLSLT5vIs8qR5vxy+500BIjDYL/snhlRj4HnP3dzPmXsl+\n' +
+      'NfKSG9TYfloTWBRlTtOmrG5QK9y3cNz3RkGon1CyiVm48UoHj0dC7Y6QIL2pnFb5\n' +
+      'E+wK1ACt0F3oH8cUJE4hXlxyhQKBgQDPrHkuqmuKOD2tW5y/rKIUTRIzaUMznpTH\n' +
+      'fCEhu7I2w8IGj1Cg5zh0xHZpVJPns52FNISResvYJEl0bjfuq5aWlGadAhKZnyi0\n' +
+      'vaKWxq6PUGYcHPtPn2mgFruWgLfCST5HMXNoOmZnRJ5XCoG/0SpkpQL7uVxdL5FE\n' +
+      'd2XVJvnDPwKBgQDnf/k/+QLrP3VeBHXWFotbhN1cZtrRpSKo+x6dDYomuk8uzNzW\n' +
+      'vif0yQOTwZBVBE3OdkKcGQqFe9vnCLPK1sVgz3yyDLoiSUgaIGRfFmMLPkzeGDAW\n' +
+      'YGOwwyHgRBAPUmDfR6RA9UWkxUsaCi+ptTZ+1dLkvIQQWMs7eOLJxROPxQKBgC9f\n' +
+      'os04ZwvCxJ8VNO6sjNSguDQuIPxXgoz3Df/J919yzIH+k2ABHunUH0br2kmSua43\n' +
+      'Sr59Cp8lt/PtaKjzT4jOFZJfdKrCmeCRGdPXQt7CcmibW+DP9qJkTSqJ9mFFoAVR\n' +
+      'jIgMOmv4RT6J0QtWxZrr4YBWynEfdr3tlrdHCta1AoGAZx95n01nZjkvCiJ/EsCe\n' +
+      'LYgwX5mLBb/oV8krVhAcDeib6lDPP9jrDha4DzzWe9Av1IwN/czsmk6RhrB3BG9Y\n' +
+      'qwZj3Pl5xK9iZ1Xe97qeZCfoMxIhJD0zgp5O/tReDPnOgz3Tgzpk1FWwPHpNzmb+\n' +
+      'kO3p67hxcVl9k1YjkWVLK6o=\n' +
+      '-----END PRIVATE KEY-----\n',
+  },
+};
+
+const publicKey = process.env.PUBLIC_KEY;
+const privateKey = process.env.PRIVATE_KEY;
+
+// const publicKey = keys.keyPair.publicKey;
+// const privateKey = keys.keyPair.privateKey;
 
 // Ejemplo de uso
 const dataToEncrypt = { message: 'Hola, este es un mensaje secreto' };
